1a96e3ed1965eee8af912aea3600d29140b2bb72
Amanda Koster <amandakoster@gmail.com>
2018-06-27 12:18:33 -0700

    added link to repo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import ToDoApp from "./ToDoApp";
-import  code  from "./"
 import "./App.css";
 
 class App extends Component {
--- src/ToDoApp.js
+++ src/ToDoApp.js
@@ -1,74 +1,93 @@
-import React from 'react';
+import React from "react";
 
 const Title = () => {
-  return( <div><h1>TODO LIST</h1></div> )
-}
-
-const ToDoItem = ({todo, remove}) => {
-  return (<li 
-            style={{ listStyleType: "none" }}   
-            onClick={() => {remove(todo.id)}}>
-            {todo.text}
-          </li>
-  )
-}
-
-const ToDoItemList = ({todos, remove}) => {
-  let list = todos.map((todo) => {
-    return (<ToDoItem 
-              todo={todo} 
-              key={todo.id} 
-              remove={remove}/>)    
-  })
-  return (<ul>{list}</ul>)
-}
-
-const ToDoItemForm = ({todoAdd}) => {
-  let input  
   return (
     <div>
-      <input placeholder=" add a todo here " ref={node => {input=node}} />
-      <button onClick={() => {
-          todoAdd(input.value)
-          input.value = ""
-        }}>+</button>
-    </div>)
-}
+      <h1>TODO LIST</h1>
+    </div>
+  );
+};
+
+const ToDoItem = ({ todo, remove }) => {
+  return (
+    <li
+      style={{ listStyleType: "none" }}
+      onClick={() => {
+        remove(todo.id);
+      }}
+    >
+      {todo.text}
+    </li>
+  );
+};
+
+const ToDoItemList = ({ todos, remove }) => {
+  let list = todos.map(todo => {
+    return <ToDoItem todo={todo} key={todo.id} remove={remove} />;
+  });
+  return <ul>{list}</ul>;
+};
+
+const ToDoItemForm = ({ todoAdd }) => {
+  let input;
+  return (
+    <div>
+      <input
+        placeholder=" add a todo here "
+        ref={node => {
+          input = node;
+        }}
+      />
+      <button
+        onClick={() => {
+          todoAdd(input.value);
+          input.value = "";
+        }}
+      >
+        +
+      </button>
+    </div>
+  );
+};
 
 window.id = 0;
 
-class ToDoApp extends React.Component{
-  constructor(props){
-    super(props); 
-    this.state = { data: [], done: false}
+class ToDoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: [], done: false };
   }
 
-  todoAdd(item){
-    const todo = {text: item, id: window.id++}
-    this.state.data.push(todo)
-    this.setState({ data: this.state.data })
+  todoAdd(item) {
+    const todo = { text: item, id: window.id++ };
+    this.state.data.push(todo);
+    this.setState({ data: this.state.data });
   }
 
-  handleRemove(id){
-    const remainder = this.state.data.filter((todo) => {
-      if(todo.id !== id) return todo;
-    })
+  handleRemove(id) {
+    const remainder = this.state.data.filter(todo => {
+      if (todo.id !== id) return todo;
+    });
     this.setState({ data: remainder });
   }
 
   render() {
-    return(
+    return (
       <div>
-      <Title />
-      <ToDoItemForm todoAdd={this.todoAdd.bind(this)} />
-      <p>You have {this.state.data.length} todo&#39;s. Click each one to delete it.</p>
-      <ToDoItemList
-        todos={this.state.data}
-        remove={this.handleRemove.bind(this)} />
+        <Title />
+        <ToDoItemForm todoAdd={this.todoAdd.bind(this)} />
+        <p>
+          You have {this.state.data.length} todo&#39;s. Click each one to delete
+          it.
+        </p>
+        <ToDoItemList
+          todos={this.state.data}
+          remove={this.handleRemove.bind(this)}
+        />
+        <a href="https://github.com/amandakoster/todo">link to repo</a>
       </div>
-    )
+    );
   }
 }
 
 export default ToDoApp;
-
